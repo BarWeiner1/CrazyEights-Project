eaa7b676384ebdacd324a15569b44667264d1a58
barweiner <King.bar.weiner@gmail.com>
2019-11-30 17:52:46 -0800

    Created JavaDocs for accessibility

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"CrazyEightsGame"},{"p":"<Unnamed>","l":"CrazyEightsRunner"},{"p":"<Unnamed>","l":"Deck"},{"p":"<Unnamed>","l":"Hand"},{"p":"<Unnamed>","l":"TextIO"}]
